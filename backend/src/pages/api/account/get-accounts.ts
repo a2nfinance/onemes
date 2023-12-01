ebca1351b52a85c0533c7df6d7daf718d2915da9
levia2n <levi@a2n.finance>
2023-12-01 14:40:22 +0700

    update db api

---
+++ backend/src/pages/api/account/get-accounts.ts
@@ -0,0 +1,33 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import connect from '@/database/connect';
+import type { NextApiRequest, NextApiResponse } from 'next'
+import Account from "@/database/models/account";
+type Data = {
+    success: boolean,
+    accounts?: any[]
+}
+
+async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    try {
+
+        // need to validate
+        if (req.body) {
+            // search data from account and request
+
+
+            const accounts = await Account.find({ wallet_address: req.body.walletAddress });
+            // aggregate data
+            res.status(200).send({ success: true, accounts: accounts });
+        } else {
+            res.status(422).send({ success: false });
+        }
+
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
+}
+
+export default connect(handler);
