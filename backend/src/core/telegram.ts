41c4a2bfd3d585d2280295cc0a549653ee6d6b96
levia2n <levi@a2n.finance>
2023-12-02 22:48:17 +0700

    Update Telegram Notu

--- backend/src/core/telegram.ts
+++ backend/src/core/telegram.ts
@@ -3,11 +3,13 @@ import dotenv from "dotenv"
 dotenv.config();
 
 const token = process.env.API_TOKEN;
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token, { polling: false });
 export const sendNoti = (requestId: string, sender: string, status: number) => {
+
     if(status) {
-        bot.sendMessage(sender, `Your token is transfered successful  (#${requestId})`)
+        bot.sendMessage(sender, `Your token is transfered successful  (#${requestId})`);
+       
     } else {
-        bot.sendMessage(sender, `Fail to transfer token (#${requestId}), please try again!`)
+        bot.sendMessage(sender, `Fail to transfer token (#${requestId}), please try again!`);
     }
 }
