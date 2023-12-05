6ce7f62e5edfb4557c52ca1e7f55f1dcfc55ecd7
levia2n <levi@a2n.finance>
2023-12-05 21:37:11 +0700

    command to start bot

--- backend/src/core/telegram.ts
+++ backend/src/core/telegram.ts
@@ -8,7 +8,7 @@ export const sendNoti = (requestId: string, sender: string, status: number) => {
 
     if(status) {
         bot.sendMessage(sender, `Your token transfer was successful with the request ID #${requestId}`);
-       
+
     } else {
         bot.sendMessage(sender, `Token transfer with request ID #${requestId} failed. Please attempt the transfer again.`);
     }
