6b5e8ed57f6acce031b5db486c5daca0c7af1bca
levia2n <levi@a2n.finance>
2023-12-09 14:56:18 +0700

    change test data

--- contracts/args.js
+++ contracts/args.js
@@ -1,5 +1,5 @@
 module.exports = [
-    ["levi10.onemes", "levi.test@a2n.finance", "+84915273032", "@levi", "", true],
+    ["levi10.onemes", "levi.test@a2n.finance", "+13243432423", "@levi", "", true],
     "0xd0daae2231e9cb96b94c8512223533293c3693bf",
     "0x779877A7B0D9E8603169DdbD7836e478b4624789",
     "0xAa16E82f0d6a5f3Cd2042F23E57aDc508200153b",
